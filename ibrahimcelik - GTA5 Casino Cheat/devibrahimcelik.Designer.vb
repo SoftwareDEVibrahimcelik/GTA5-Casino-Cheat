c35f3ef8f4bbfbb7ce2bcd8fbae9d2b66eb87159
IBRAHIM CELIK <168642694+SoftwareDEVibrahimcelik@users.noreply.github.com>
2024-05-12 16:15:48 +0300

    Add files via upload
    GTA5 Casino Cheat, it allows you to get 2 million each time.
    
    
    Software Developer IBRAHIM CELIK
    Github: @SoftwareDEVibrahimcelik
    Instagram: [https://www.instagram.com/softwaredevic/p/C2pOtE4MN10/](url)
    YouTube: [https://www.youtube.com/@devibrahimcelik](url)
    Github Link: [https://github.com/SoftwareDEVibrahimcelik](url)
    
    Bitcoin Address: 3H2iUqWmQ2RGTYDjJwceVeEFT8XMTafjrk

---
+++ ibrahimcelik - GTA5 Casino Cheat/devibrahimcelik.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class devibrahimcelik
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(25, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(246, 76)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Casino Cheat"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(376, 107)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "devibrahimcelik"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.White
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 170)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(51, 13)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "YouTube"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.LinkColor = System.Drawing.Color.White
+        Me.LinkLabel2.Location = New System.Drawing.Point(121, 170)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(43, 13)
+        Me.LinkLabel2.TabIndex = 4
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Discord"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.LinkColor = System.Drawing.Color.White
+        Me.LinkLabel3.Location = New System.Drawing.Point(211, 170)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(77, 13)
+        Me.LinkLabel3.TabIndex = 5
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "Donate Bitcoin"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(96, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "GTA 5 - Status"
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'devibrahimcelik
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(300, 206)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LinkLabel3)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "devibrahimcelik"
+        Me.Text = "devibrahimcelik - GTA5 CHEAT"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel3 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+End Class
---
+++ ibrahimcelik - GTA5 Casino Cheat/devibrahimcelik.vb
@@ -0,0 +1,61 @@
+﻿Public Class devibrahimcelik
+
+    'Reedited & updated for 64 bit by software developer IBRAHIM CELIK
+    'YouTube: @devibrahimcelik | : https://www.youtube.com/@devibrahimcelik
+    'Github: https://github.com/SoftwareDEVibrahimcelik
+    'Instagram: softwaredevic
+    'Donate Bitcoin Address: 3H2iUqWmQ2RGTYDjJwceVeEFT8XMTafjrk
+
+
+    Private Sub devibrahimcelik_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        On Error Resume Next
+        WriteDMAInteger("GTA5", Label1.Text + &H29BD898, {&H38, &H18, &H10, &H8, &H1668, &H1538, &H970}, 1000000, 7)
+        WriteDMAInteger("GTA5", Label1.Text + &H29BD898, {&H98, &H18, &H18, &H8, &H968, &H108, &H980}, 1000000, 7)
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("https://www.youtube.com/channel/@devibrahimcelik")
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        System.Diagnostics.Process.Start("https://discord.com/invite/HBBkbneJ49")
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Clipboard.SetDataObject("3H2iUqWmQ2RGTYDjJwceVeEFT8XMTafjrk")
+        MsgBox("Copied bitcoin address")
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim p() As Process
+        p = Process.GetProcessesByName("GTA5")
+
+
+        If p.Count > 0 Then
+            Label2.ForeColor = Color.Lime
+            Label2.Text = "GTA 5  is working"
+        Else
+            Label2.Text = "GTA 5  is NOT working"
+        End If
+
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        On Error Resume Next
+        Dim ibrahim_celik53 As Process = Process.GetProcessesByName("GTA5")(0)
+        For Each moz As System.Diagnostics.ProcessModule In ibrahim_celik53.Modules
+            If moz.FileName.IndexOf("GTA5.exe") <> -1 Then
+                Label1.Text = moz.BaseAddress.ToString
+            End If
+        Next
+
+        Timer2.Interval = 10000 ''10 Saniye sonrasında base adresi alıcak. Yani bu bir looptur.
+    End Sub
+End Class
